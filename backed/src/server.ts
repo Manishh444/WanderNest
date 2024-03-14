fe5572c4f2930af46dd88a0388de645045a4e906
Manishh444 <manishbhardwaj131149@gmail.com>
2024-03-14 08:27:08 +0530

    docker file updated

--- backed/src/server.ts
+++ backed/src/server.ts
@@ -3,6 +3,10 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res)=>{
+  res.send("<h1>hello World</h1>")
+})
+
 app.listen(5000, () => {
   console.log("hello from dev server with nodemon server");
 });
